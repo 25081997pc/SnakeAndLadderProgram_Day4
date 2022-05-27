3ece76c958e1017525a395c9188dc2e737c7dd75
25081997pc <chavanpooja2508@gmail.com>
2022-05-27 15:52:44 +0530

    Game Started with Single Player

---
+++ GameStart.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    public class GameStart
+    {
+       
+        public static void GameIsStart()
+        {
+            int players = 1;
+            int Player1Position = 0;
+
+            Console.WriteLine("Player1 Position :" + Player1Position);
+        }
+         
+    }
+}
